7918cec5041f293e616ad33771314274beabffa5
vaibhavkumar1 <vaibhav.kumar_cs18@gla.ac.in>
2020-08-14 16:23:51 +0530

    Updates

---
+++ precache-manifest.a54adecf966d7dddb874edbee31f8a2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83326bc1381f90681eadcdb614a2f66",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "2aab2d8199a9f5d3e7a5",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64a6b63e713d47ed482e",
+    "url": "/hello/static/js/2.6a70fac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.6a70fac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aab2d8199a9f5d3e7a5",
+    "url": "/hello/static/js/main.fe7d55d6.chunk.js"
+  },
+  {
+    "revision": "543a3730c717a6d3edaf",
+    "url": "/hello/static/js/runtime-main.eda9fcde.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
